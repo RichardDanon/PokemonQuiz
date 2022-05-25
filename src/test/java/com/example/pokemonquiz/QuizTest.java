aa34214c3923e78db3007c73c8c1d8c300d77c95
RichardDanon <104465518+RichardDanon@users.noreply.github.com>
2022-05-24 20:29:17 -0400

    Writing the tests for the Quiz Class

--- src/test/java/com/example/pokemonquiz/QuizTest.java
+++ src/test/java/com/example/pokemonquiz/QuizTest.java
@@ -3,6 +3,7 @@ package com.example.pokemonquiz;
 import javafx.application.Platform;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CountDownLatch;
@@ -11,8 +12,12 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuizTest {
+    Quiz quiz ;
+    Question q1;
+    Question q2;
 
     //Borrowed to make the test work
+    //https://stackoverflow.com/questions/11385604/how-do-you-unit-test-a-javafx-controller-with-junit
     @BeforeAll
     public static void setUpJavaFXRuntime() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
@@ -22,6 +27,19 @@ class QuizTest {
         latch.await(5, TimeUnit.SECONDS);
     }
 
+    @BeforeEach
+    void init_setUpQuiz(){
+        q1 = new Question("Images/Absol.jpg", "Absol");
+        q2 = new Question("Images/Charizard.jpg", "Charizard");
+
+        quiz = new Quiz(2);
+
+        quiz.addQuestion(q1);
+        quiz.addQuestion(q2);
+    }
+
+    //Borrowed to make the test work
+    //https://stackoverflow.com/questions/11385604/how-do-you-unit-test-a-javafx-controller-with-junit
     @AfterAll
     public static void tearDownJavaFXRuntime() throws InterruptedException {
         Platform.exit();
@@ -29,15 +47,20 @@ class QuizTest {
 
     @Test
     void getNextQuestion_ReturnsQuestionsInCorrectOrder() {
-        Question q1 = new Question("Images/Absol.jpg", "Absol");
-        Question q2 = new Question("Images/Charizard.jpg", "Charizard");
-
-        Quiz quiz = new Quiz(2);
-
-        quiz.addQuestion(q1);
-        quiz.addQuestion(q2);
 
         assertEquals(q2,quiz.getNextQuestion());
+    }
+
+    @Test
+    void currentTest_ReturnsFirstQuestion(){
+        assertEquals(q1,quiz.currentQuestion());
+    }
+    @Test
+    void FinalScore_keepsTheOrginalHighScoreWhenItIsNotHighEnough(){
+        Score score = new Score();
+        quiz.setFinalScore(score);
+
+        assertNotEquals(score,quiz.getHighScore());
     }
 
 }
